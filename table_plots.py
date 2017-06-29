fc3d78cd82855bc8be02837371bbabf87a6c459c
Rafael Pereira <rflprr@users.noreply.github.com>
2017-06-28 22:13:29 -0500

    Write datapackage.json

--- table_plots.py
+++ table_plots.py
@@ -12,10 +12,25 @@ import pandas as pd
 def plots(input_path, output_path='/pfs/out'):
     dp = datapackage.DataPackage(descriptor=os.path.join(input_path,
                                                          'datapackage.json'))
+
+    dp_out = datapackage.DataPackage()
+    dp_out['name'] = 'nba-plots'
+    dp_out['title'] = 'nba-plots'
+    dp_out['description'] = 'My NBA Plots'
+    dp_out['x-visibility'] = 'PRIVATE'
+    dp_out['licenses'] = [
+        {'name': 'Other'}
+    ]
+    dp_out['resources'] = []
     for r in dp.descriptor['resources']:
         df = pd.read_csv(r['path'])
         fig = df.plot().get_figure()
-        fig.savefig(os.path.join(output_path, '{}.png'.format(r['name'])))
+        plot_name = '{}.png'.format(r['name'])
+        fig.savefig(os.path.join(output_path, plot_name))
+        dp_out['resources'].append({'name': plot_name, 'path': plot_name})
+
+    with open(os.path.join(output_path, 'datapackage.json'), 'w') as f:
+        f.write(dp_out.to_json())
 
 
 if __name__ == '__main__':
